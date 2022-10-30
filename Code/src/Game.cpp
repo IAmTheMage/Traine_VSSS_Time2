f791c16dffa793985bb3a273db6085f48053df02
IAmTheMage <gustavogoncalvessilva2003@outlook.com>
2022-10-30 16:21:16 -0300

    Add positions

--- Code/src/Game.cpp
+++ Code/src/Game.cpp
@@ -9,8 +9,6 @@ Game::Game(int argc, char** argv) {
     this->strategy = new StrategyManager();
     this->strategy->addStrategy("Primeira estrategia",_strategy);
     this->strategy->setStrategy("Primeira estrategia");
-    std::ifstream ifs("config.json");
-    config = json::parse(ifs);
     std::cout << "Size w: " << config["gameWidth"] << std::endl << std::endl << "Game height: " << config["gameHeight"] << std::endl << std::endl;
 }
 
@@ -22,10 +20,20 @@ Game::~Game() {
 }
 
 void Game::instance() {
+    std::ifstream ifs("config.json");
+    config = json::parse(ifs);
     this->ball = std::make_shared<Object<void*>>();
     ball->pos = {75, 65};
     ball->speed = {0.0f, 0.0f};
     for(int i = 0; i < 3; i++) {
+        team1Robots[i].pos = {config["robotsPositions"]["team1"][i]["x"], config["robotsPositions"]["team1"][i]["y"]};
+        team2Robots[i].pos = {config["robotsPositions"]["team2"][i]["x"], config["robotsPositions"]["team2"][i]["y"]};
+    }
+    for(int i = 0; i < 3; i++) {
+        std::cout << "Posicao do robo " << i << " do time 1 " << "{ " << team1Robots[i].pos.x << " , " << team1Robots[i].pos.y << " }" << std::endl << std::endl;
+    }
+    for(int i = 0; i < 3; i++) {
+        std::cout << "Posicao do robo " << i << " do time 2 " << "{ " << team2Robots[i].pos.x << " , " << team2Robots[i].pos.y << " }" << std::endl << std::endl;
     }
 }
 
@@ -35,8 +43,8 @@ void Game::run() {
     std::cout << "Pause condition: " << pauseCondition << std::endl << std::endl;
     bool isRunning = score1 < pauseCondition && score2 < pauseCondition;
     while(isRunning) {
-        display();
-        this->strategy->deduce();
+        //display();
+        //this->strategy->deduce();
         isRunning = score1 < pauseCondition && score2 < pauseCondition;
     }
 }

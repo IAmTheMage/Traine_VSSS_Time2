1023f66ee3bfd88d43c3b270b34d10b04d4be0d7
Rtmimura <rtmimura@gmail.com>
2022-11-07 18:39:31 -0300

    Estrategia do atacante

--- Code/include/StrikerStrategy.h
+++ Code/include/StrikerStrategy.h
@@ -26,27 +26,81 @@ class StrikerStrategy : public BaseStrategy {
         }
 
         void striker() {
-            float th, d, y;
-            int places[3];
+            float th, d, x, y;
+            int places[3], i;
 
             th = Utils::getAngle(strikerBot->pos, ball->pos);
             d = Utils::getDist(strikerBot->pos, ball->pos);
             y = 0;
 
+            RectCollider col1 = {strikerBot->pos.x, strikerBot->pos.y, 4, 4};
+            RectCollider col2 = {ball->pos.x, ball->pos.y, 2.135, 2.135};
+
+            
+            if (movement->lookAt(myTeamRobots[BOT], th, 0.05)) {
+                movement->chaseS(myTeamRobots[BOT], ball->pos, 1, 1);
+            }
+
             places[0] = Utils::getQuadrant(ball->pos);
-            if (places[0] != 9) {
+            if (places[0] != 3 && places[0] != 9) {
                 for (int i=0; i<3; i++) {y += anotherTeamRobots[i].pos.y;}
                 y /= 3;
 
-                if (movement->lookAt(myTeamRobots[BOT], th, 0.05)) {
-                    movement->chaseS(myTeamRobots[BOT], ball->pos, 1, 1);
-                }
-
-                RectCollider col1 = {strikerBot->pos.x, strikerBot->pos.y, 4, 4};
-                RectCollider col2 = {ball->pos.x, ball->pos.y, 2.135, 2.135};
                 if (Collision::checkCollision(col1, col2)) {
                     if (y > 65) {movement->kick(myTeamRobots[BOT], *ball, {125, 22});}
                     else {movement->kick(myTeamRobots[BOT], *ball, {125, 108});}
+                }
+            }
+
+            else if (places[0] == 3) {
+                if (Collision::checkCollision(col1, col2)) {
+                    y = 0;
+                    for (i=0; i<3; i++) {
+                        if (Utils::getQuadrant(anotherTeamRobots[i].pos) == -2) {break;}
+                    }
+
+                    for (int j=0; j<3; j++) {
+                        if (j==i) {continue;}
+                        x += anotherTeamRobots[j].pos.y;
+                        y += anotherTeamRobots[j].pos.y;
+                    }
+                    x /= 2; y /= 2;
+
+                    places[1] = Utils::getQuadrant({x, y});
+                    if (places[1] != 6 && places[1] != 3) {
+                        x = anotherTeamRobots[i].pos.x + 15;
+                        if (anotherTeamRobots[i].pos.y > 65) {y = anotherTeamRobots[i].pos.y - 10;}
+                        else {y = anotherTeamRobots[i].pos.y + 10;}
+
+                        
+                        movement->kick(myTeamRobots[BOT], *ball, {x, y});
+                    }
+                }
+            }
+
+            else if (places[0] == 9) {
+                if (Collision::checkCollision(col1, col2)) {
+                    y = 0;
+                    for (i=0; i<3; i++) {
+                        if (Utils::getQuadrant(anotherTeamRobots[i].pos) == -2) {break;}
+                    }
+
+                    for (int j=0; j<3; j++) {
+                        if (j==i) {continue;}
+                        x += anotherTeamRobots[j].pos.y;
+                        y += anotherTeamRobots[j].pos.y;
+                    }
+                    x /= 2; y /= 2;
+
+                    places[1] = Utils::getQuadrant({x, y});
+                    if (places[1] != 6 && places[1] != 9) {
+                        x = anotherTeamRobots[i].pos.x + 15;
+                        if (anotherTeamRobots[i].pos.y > 65) {y = anotherTeamRobots[i].pos.y - 10;}
+                        else {y = anotherTeamRobots[i].pos.y + 10;}
+
+                        
+                        movement->kick(myTeamRobots[BOT], *ball, {x, y});
+                    }
                 }
             }
             
